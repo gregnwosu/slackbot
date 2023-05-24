db06f51c743028e105f9621b34f1140b54251a7a
greg <greg.nwosu@gmail.com>
2023-05-24 03:30:26 +0100

    terraform ipdates

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -8,14 +8,14 @@ resource "azurerm_service_plan" "serviceplan" {
 
 }
 
-resource "azurerm_linux_web_app" "web_app" {
+resource "azurerm_linux_web_app" "slackbotwebapp" {
 
-    name = "webapp"
+    name = "slackbotwebapp"
     resource_group_name = azurerm_resource_group.LangChain-Experiments.name 
     location = var.resource_group_location
     service_plan_id = azurerm_service_plan.serviceplan.id
     site_config {
-        always_on = true
+        always_on = false
         
          application_stack{
             python_version = "3.11"
