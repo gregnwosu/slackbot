0c65810ec9759710d809543aa13d0ce6885ae2f5
greg <greg.nwosu@gmail.com>
2023-05-28 00:33:29 +0100

    feat: :bricks: add automated terraform building

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -14,7 +14,7 @@ resource "azurerm_linux_web_app" "slackbotwebapp" {
     resource_group_name = azurerm_resource_group.LangChain-Experiments.name 
     location = var.resource_group_location
     service_plan_id = azurerm_service_plan.serviceplan.id 
-     app_settings                      = {
+    app_settings                      = {
           "SCM_DO_BUILD_DURING_DEPLOYMENT" = "1" 
           "SLACK_BOT_TOKEN"="${var.SLACK_BOT_TOKEN}"
           "SLACK_SIGNING_SECRET"="${var.SLACK_SIGNING_SECRET}"
@@ -23,18 +23,15 @@ resource "azurerm_linux_web_app" "slackbotwebapp" {
           "OPENAI_API_KEY"="${var.OPENAI_API_KEY}"
 
           #get variable from the environment
-
-          
           
         }
     site_config {
-        always_on = false
-        app_command_line = "startup.txt"
-         application_stack{
+      always_on = false
+      app_command_line = "startup.txt"
+      application_stack{
             python_version = "3.11"
-         } 
-   
-}
+      } 
+    }
 
     }
-   
+  
