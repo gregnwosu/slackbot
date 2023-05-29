ddfde8cc5f5a9fb86654af2cb880c5af5f363c8b
greg <greg.nwosu@gmail.com>
2023-05-29 01:35:06 +0100

    feat: :art: add azure storage container terraform backend

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,13 +1,13 @@
-resource "azurerm_storage_account" "storage_account" {
-  name                     = "slackbotstorageaccount"
+resource "azurerm_storage_account" "terraform_state" {
+  name                     = "gnwosutfstatestorageacc"
   resource_group_name      = azurerm_resource_group.LangChain-Experiments.name
   location                 = azurerm_resource_group.LangChain-Experiments.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "storage_container" {
-  name                  = "slackbotstoragecontainer"
-  storage_account_name  = azurerm_storage_account.storage_account.name
+resource "azurerm_storage_container" "terraform_state" {
+  name                  = "gnwosutfstatestoragecont"
+  storage_account_name  = azurerm_storage_account.terraform_state.name
   container_access_type = "private"
 }
