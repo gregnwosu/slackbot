076d8b299a09b6f494ba0c10f68787f002732ea4
greg <greg.nwosu@gmail.com>
2023-06-01 02:47:21 +0100

    feat: :sparkles: more tools starting with zapier tool

--- slack/functions.py
+++ slack/functions.py
@@ -1,16 +1,33 @@
+from typing import Any, Coroutine
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
-from dotenv import find_dotenv, load_dotenv
+from langchain.agents import initialize_agent, load_tools
+from langchain.agents.agent import AgentExecutor
+from langchain.agents.agent_toolkits import ZapierToolkit
+from langchain.llms import OpenAI 
+from langchain.agents.agent_types import AgentType
+from langchain.memory import ConversationBufferMemory 
+from langchain.utilities.zapier import ZapierNLAWrapper
+import numpy as np
+import openai
+import sounddevice as sd
+import soundfile as sf
+import tempfile
+from langchain.tools import BaseTool
+from langchain.utilities.zapier import ZapierNLAWrapper
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
+import os 
+import elevenlabs
 
-load_dotenv(find_dotenv())
+from cachetools import TTLCache
 
 
-def draft_email(user_input, name="Dave"):
+#TODO make this into a tool.
+def draft_email(user_input, name="Dave") -> str:
     chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1)
 
     template = """
@@ -38,6 +55,77 @@ def draft_email(user_input, name="Dave"):
     )
 
     chain = LLMChain(llm=chat, prompt=chat_prompt)
-    response = chain.run(user_input=user_input, signature=signature, name=name)
+    return chain.run(user_input=user_input, signature=signature, name=name)
+    
 
-    return response
+   
+
+
+elevenlabs.set_api_key(os.environ["ELEVENLABS_API_KEY"])
+
+
+
+
+
+@TTLCache(maxsize=100, ttl=300)
+def openai_llm():
+    return OpenAI(temperatture=0, openai_api_key=os.environ["OPENAI_API_KEY"])
+
+
+def record_audio(duration, fs, channels):
+    print("Recording...")
+    recording = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
+    sd.wait()
+    print("Finished recording.")
+    return recording
+
+
+def transcribe_audio(recording, fs):
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_audio:
+        sf.write(temp_audio.name, recording, fs)
+        temp_audio.close()
+        with open(temp_audio.name, "rb") as audio_file:
+            transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        os.remove(temp_audio.name)
+    return transcript["text"].strip()
+
+@TTLCache(maxsize=100, ttl=300)
+def agi() -> AgentExecutor:
+    open_ai_llm = openai_llm()
+    memory = ConversationBufferMemory(memory_key="slackbot-chat-history")
+    zapier = ZapierNLAWrapper(zapier_nla_api_key=os.environ["ZAPIER_API_KEY"], zapier_nla_oauth_access_token=None)
+    zapier_toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
+    tools = zapier_toolkit.get_tools() + load_tools(["human"])
+    return initialize_agent(tools, open_ai_llm, memory=memory, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True)
+    
+
+
+def play_generated_audio(text, voice="Bella", model="eleven_monolingual_v1"):
+    audio = elevenlabs.generate(text=text, voice=voice, model=model)
+    elevenlabs.play(audio)
+
+class EmailTool(BaseTool):
+    name = "send email via gmail"
+    description = "send email via gmail"
+
+    def _run(self, text:str) -> str:
+        """Use the tool"""
+        return draft_email(text)
+    
+
+    async def _arun(self, *args: Any, **kwargs: Any) -> Coroutine[Any, Any, Any]:
+        raise NotImplementedError()
+        #return await super()._arun(*args, **kwargs)
+    
+
+    # while True:
+    #     print("Press spacebar to start recording.")
+    #     keyboard.wait("space")  # wait for spacebar to be pressed
+    #     recorded_audio = record_audio(duration, fs, channels)
+    #     message = transcribe_audio(recorded_audio, fs)
+    #     print(f"You: {message}")
+    #     assistant_message = agent.run(message)
+    #     play_generated_audio(assistant_message)
+
+    #TODO just need to use the slack voice message to send and receive audio
+
