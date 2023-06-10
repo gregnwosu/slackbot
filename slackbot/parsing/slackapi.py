27823b8ecc8e2f168a556a93be115f157f8a9052
Gregory Nwosu <193151+gregnwosu@users.noreply.github.com>
2023-06-10 17:45:28 +0100

    Revert "file info (#20)"
    This reverts commit d2e1ab522381e1b60ac552eede9ad827d8ea7ec3.

--- slackbot/parsing/slackapi.py
+++ slackbot/parsing/slackapi.py
@@ -9,10 +9,8 @@ from urllib.parse import ParseResult
 from logging import Logger
 logger = Logger(__name__)
 from slack_sdk import WebClient
-import requests 
 
 #AppMentionData
-
 
 class EventType(Enum):
     APP_MENTION = "app_mention"
@@ -95,6 +93,48 @@ class FileEvent(pydantic.BaseModel):
         assert response["ok"], f"Slack API call failed with error: {response['error']}"
         return FileInfo(**response["file"])
 
+
+{'token': 'waNounRnAWVeA53FlYyaPaP8', 'team_id': 'T058PNE2HKP', 'context_team_id': 'T058PNE2HKP', 'context_enterprise_id': None, 'api_app_id': 'A058SM2MXS6', 
+ 'event': {'type': 'file_change', 'file_id': 'F05BEHD7BRT', 'user_id': 'U058V5QTW12', 'file': {'id': 'F05BEHD7BRT'}, 'event_ts': '1686181314.046100'}, 'type': 'event_callback', 'event_id': 'Ev05BHBXRXNF', 'event_time': 1686181314, 'authorizations': [{'enterprise_id': None, 'team_id': 'T058PNE2HKP', 'user_id': 'U058V6AG10C', 'is_bot': True, 'is_enterprise_install': False}], 'is_ext_shared_channel': False, 'event_context': '4-eyJldCI6ImZpbGVfY2hhbmdlIiwidGlkIjoiVDA1OFBORTJIS1AiLCJhaWQiOiJBMDU4U00yTVhTNiIsImZpZCI6IkYwNUJFSEQ3QlJUIn0'}
+
+#File Shared Event
+
+{'id': 'F05BL5K8RLG', 'created': 1685914831, 'timestamp': 1685914831, 'name': 'audio_message.webm', 
+    'title': 'audio_message.webm', 'mimetype': 'audio/webm', 'filetype': 'webm', 'pretty_type': 'WebM',
+    'user': 'U058V5QTW12', 'user_team': 'T058PNE2HKP', 'editable': False, 'size': 137519, 'mode': 'hosted',
+        'is_external': False, 'external_type': '', 'is_public': True, 'public_url_shared': False, 'display_as_bot': False, 
+        'username': '', 'subtype': 'slack_audio', 
+        'transcription': {'status': 'complete', 'locale': 'en-US', 
+                        'preview': {'content': 'Back to just, you know, the work flow here really quick. This is a great starting point. And then after this way you can do, you can have another.',
+                                    'has_more': False}
+                        },
+    'url_private': 'https://files.slack.com/files-tmb/T058PNE2HKP-F05BL5K8RLG-c298d7bc9c/audio_message_audio.mp4', 
+    'url_private_download': 'https://files.slack.com/files-tmb/T058PNE2HKP-F05BL5K8RLG-c298d7bc9c/download/audio_message_audio.mp4', 
+    'vtt': 'https://files.slack.com/files-tmb/T058PNE2HKP-F05BL5K8RLG-c298d7bc9c/file.vtt?_xcb=a3a01', 
+    'duration_ms': 8458, 
+    'aac': 'https://files.slack.com/files-tmb/T058PNE2HKP-F05BL5K8RLG-c298d7bc9c/audio_message_audio.mp4',
+    'audio_wave_samples': [12, 8, 8, 3, 4, 5, 15, 32, 4, 11, 24, 27, 21, 4, 3, 3, 3, 3, 6, 27, 15, 11, 3, 3, 3, 10, 38, 29, 31, 4, 6, 59, 30, 4, 8, 54, 49, 3, 14, 35, 39, 36, 32, 7, 93, 36, 5, 14, 13, 4, 4, 3, 4, 4, 4, 4, 14, 7, 93, 100, 16, 38, 55, 14, 75, 44, 4, 6, 31, 48, 29, 12, 4, 8, 23, 13, 7, 18, 14, 12, 10, 13, 7, 12, 30, 2, 18, 14, 5, 10, 17, 15, 9, 38, 47, 49, 23, 44, 41, 57], 
+    'media_display_type': 'audio', 
+    'permalink': 'https://ai-experimentshq.slack.com/files/U058V5QTW12/F05BL5K8RLG/audio_message.webm',
+    'permalink_public': 'https://slack-files.com/T058PNE2HKP-F05BL5K8RLG-80cd5cc6fa', 
+    'is_starred': False,
+    'shares': {'public': {'C0595A85N4R': [
+                                {'reply_users': [], 'reply_users_count': 0, 'reply_count': 0, 'ts': '1685914836.857269', 
+                                'channel_name': 'admin', 'team_id': 'T058PNE2HKP', 'share_user_id': 'U058V6AG10C'}, 
+                                {'reply_users': [], 'reply_users_count': 0, 'reply_count': 0, 'ts': '1685914835.606609', 
+                                'channel_name': 'admin', 'team_id': 'T058PNE2HKP', 'share_user_id': 'U058V5QTW12'}
+                                ]
+                        }
+            },
+                                                                                                                
+                                                                                                                
+    'channels': ['C0595A85N4R'], 
+    'groups': [], 
+    'ims': [], 
+    'has_more_shares': False, 
+    'has_rich_preview': False, 
+    'file_access': 'visible', 
+    'comments_count': 0}
 
 class MimeType(str, Enum):
     AUDIO_WEBM = "audio/webm"
@@ -191,14 +231,5 @@ class FileInfo(pydantic.BaseModel):
     has_rich_preview: bool
     file_access: FileAccess 
     comments_count: int
-
-    @property
-    def full_transcript(self) -> Optional[str]:
-        # return the value pointed to by the vtt url
-        if self.vtt:
-            response: requests.Response = requests.get(self.vtt)
-            response.raise_for_status()
-            return response.text()
-        return None
 
     
