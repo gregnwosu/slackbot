a628a2202cfbf85f54d436bc7b579c277b90957d
Gregory Nwosu <193151+gregnwosu@users.noreply.github.com>
2023-07-03 00:58:12 +0100

    redis tf (#27)

--- terraform/network.tf
+++ terraform/network.tf
@@ -20,20 +20,22 @@
 
 
 
-resource "azurerm_virtual_network" "LangChain-Experiments" {
-  name                = "example-network"
-  location            = azurerm_resource_group.LangChain-Experiments.location
-  resource_group_name = azurerm_resource_group.LangChain-Experiments.name
-  address_space       = ["10.0.0.0/16"]
+
+# resource "azurerm_virtual_network" "LangChain-Experiments" {
+#   name                = "example-network"
+#   location            = azurerm_resource_group.LangChain-Experiments.location
+#   resource_group_name = azurerm_resource_group.LangChain-Experiments.name
+#   address_space       = ["10.0.0.0/16"]
  
 
-  subnet {
-    name           = "backend"
-    address_prefix = "10.0.1.0/24"
-  }
+#   subnet {
+#     name           = "backend"
+#     address_prefix = "10.0.1.0/24"
+#   }
 
  
-  tags = {
-    environment = "chatbot_net"
-  }
-}
+#   tags = {
+#     environment = "chatbot_net"
+#   }
+# }
+
