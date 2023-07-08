e2ba7ec9fe9ea17e725676b988dbb9a963defc8d
greg <greg.nwosu@gmail.com>
2023-07-08 23:05:18 +0100

    feat: :sparkles: open network for redis cache to public

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -7,7 +7,7 @@ resource "azurerm_redis_cache" "bot-cache" {
   sku_name            = "Basic"
   enable_non_ssl_port = false
   minimum_tls_version = "1.2"
-  public_network_access_enabled = false
+  public_network_access_enabled = true
 
   # redis_configuration {
   #   aof_backup_enabled = false
--- tests/slackbot/test_redis.py
+++ tests/slackbot/test_redis.py
@@ -1,22 +1,17 @@
-import aioredis
+
 import pytest 
 from slackbot import app
 
 
 
 
-@pytest.mark.skip
+@pytest.mark.asyncio
 async def test_redis_cache():
-    redis = app.get_cache() 
-    # await redis.set("test", "test")
-    # actual = await redis.get("test")
-    # assert actual == "test"
-    redis.close()
+    redis = app.get_cache()
+    await redis.set("test", "nukkk")
+    actual = await redis.get("test")
+    assert actual == "nukkk"
+    await redis.close()
     
-    # async with await app.get_cache() as redis:
-    #     await redis.set("test", "test")
-    #     assert await redis.get("test") == "test"
-    #     await redis.delete("test")
-    #     assert await redis.get("test") == None
 
     
