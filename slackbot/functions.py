34c58c1a128756c5e33cbf6e5280f553524d9079
greg <greg.nwosu@gmail.com>
2023-07-10 00:51:19 +0100

    async functions and tree f thought prompt

--- slackbot/functions.py
+++ slackbot/functions.py
@@ -1,10 +1,11 @@
 from typing import Any, Coroutine
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, ConversationChain
 from langchain.agents import initialize_agent, load_tools
 from langchain.agents.agent import AgentExecutor
 from langchain.agents.agent_toolkits import ZapierToolkit
 from langchain.llms import OpenAI 
+
 from langchain.agents.agent_types import AgentType
 from langchain.memory import ConversationBufferMemory 
 from langchain.utilities.zapier import ZapierNLAWrapper
@@ -22,31 +23,37 @@ from langchain.prompts.chat import (
 )
 import os 
 import elevenlabs
-
+import aioredis
 from cachetools import TTLCache, cached
 
 
-def generate_audio(text, voice="Bella", model="eleven_monolingual_v1"):
+async def generate_audio(text,  cache: aioredis.Redis, voice="Bella", model="eleven_monolingual_v1"):
+    if cache.exists(text):
+        return cache.get(text)
     elevenlabs.set_api_key(os.environ["ELEVENLABS_API_KEY"])
     audio = elevenlabs.generate(text=text, voice=voice, model=model)
+    cache.set(text, audio)
     return audio
 
 
 #TODO make this into a tool.
-def draft_email(user_input, name="Dave") -> str:
+async def convo(user_input:str, name="Dave") -> str:
     chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1)
 
-    template = """
+    template = f"""
     
-    You are a helpful assistant that drafts an email reply based on an a new email.
-    
-    Your goal is to help the user quickly create a perfect email reply.
-    
-    Keep your reply short and to the point and mimic the style of the email so you reply in a similar manner to match the tone.
-    
-    Start your reply by saying: "Hi {name}, here's a draft for your reply:". And then proceed with the reply on a new line.
-    
-    Make sure to sign of with {signature}.
+    Imagine three different experts are answering this question.
+    They will brainstorm the answer step by step; reasoning carefully and taking all the facts into consideration..
+    All experts will write down 1 step of their thinking , then share with the group.
+    They will each critique their own response and the responses of others.
+    They critique more heavily the responses that led them to change their mind.
+    They will check their answers based on science and the laws of physics , math and logic.
+    Then all experts will go on to the next step and write down this step in thier thinking.
+    If at any time they realise that there is a flaw in their logic they will backtrack to where the flaw occured.
+    If any expert realises they're wrong at any point then they acknowledge this and backtrack to where they went wrong to start another train of thought.
+    Each expert will assign a likelihood of their current assertion being correct.
+    Continue until all experts agree on the single most likely answer.
+    The Question is: {user_input}
     
     """
 
@@ -60,8 +67,8 @@ def draft_email(user_input, name="Dave") -> str:
         [system_message_prompt, human_message_prompt]
     )
 
-    chain = LLMChain(llm=chat, prompt=chat_prompt)
-    return chain.run(user_input=user_input, signature=signature, name=name)
+    chain = ConversationChain(llm=chat, prompt=chat_prompt)
+    return await chain.arun(user_input=user_input, signature=signature, name=name)
 
 
 #elevenlabs.set_api_key(os.environ["ELEVENLABS_API_KEY"])
@@ -104,7 +111,7 @@ class EmailTool(BaseTool):
 
     def _run(self, text:str) -> str:
         """Use the tool"""
-        return draft_email(text)
+        return convo(text)
     
 
     async def _arun(self, *args: Any, **kwargs: Any) -> Coroutine[Any, Any, Any]:
