9ee25becf478f13e68e081240b1146eaccb52b87
greg <greg.nwosu@gmail.com>
2023-07-15 23:09:02 +0100

    fix sku for speech f0

--- terraform/speech.tf
+++ terraform/speech.tf
@@ -3,7 +3,7 @@ resource "azurerm_cognitive_account" "slackbot_synth" {
   location            = azurerm_resource_group.LangChain-Experiments.location
   resource_group_name = azurerm_resource_group.LangChain-Experiments.name
   kind               = "SpeechServices"
-  sku_name                 = "F1"
+  sku_name                 = "F0"
 }
 
 
