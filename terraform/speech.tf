da118f32bc49d12eb087cb4248aa9769f2554cc1
greg <greg.nwosu@gmail.com>
2023-07-16 00:02:21 +0100

    fix

--- terraform/speech.tf
+++ terraform/speech.tf
@@ -1,5 +1,5 @@
 resource "azurerm_cognitive_account" "LangChain_Experiments" {
-  name                = "LangChain-Experiments#"
+  name                = "LangChain-Experiments"
   location            = azurerm_resource_group.LangChain-Experiments.location
   resource_group_name = azurerm_resource_group.LangChain-Experiments.name
   kind               = "SpeechServices"
