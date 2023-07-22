ddd370784843d7783ddcd2c84667aaeaf0ed263a
greg <greg.nwosu@gmail.com>
2023-07-23 00:50:21 +0100

    cache secrets

--- slackbot/vault.py
+++ slackbot/vault.py
@@ -2,12 +2,14 @@ from azure.identity.aio import DefaultAzureCredential
 from azure.keyvault.secrets.aio import SecretClient
 import os
 from dotenv import load_dotenv, find_dotenv
+import async_lru
 
 load_dotenv(find_dotenv())
 
 VAULT_URL = os.environ["VAULT_URL"]
 
-
+# cache result from async function
+@async_lru(maxsize=50)
 async def get_secret(secret_name):
     async with DefaultAzureCredential() as credential:
         async with SecretClient(vault_url=VAULT_URL, credential=credential) as client:
