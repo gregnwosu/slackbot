4a0fd7dd9c6b9db0360e23c03b3ab2588ff1dde1
greg <greg.nwosu@gmail.com>
2023-07-23 00:09:14 +0100

    added cogservices keys

--- terraform/bing.tf
+++ terraform/bing.tf
@@ -1,5 +1,5 @@
-resource "azurerm_cognitive_account" "slackbot_bing_websearch" {
-  name                = "Slackbot-Bing-WebSearch"
+resource "azurerm_cognitive_account" "slackbot_azure_cogservices" {
+  name                = "Slackbot-CogServices"
   location            = azurerm_resource_group.LangChain-Experiments.location
   resource_group_name = azurerm_resource_group.LangChain-Experiments.name
   kind                = "Bing.Search.v7"
