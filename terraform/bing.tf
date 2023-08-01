4591a569bbc80c6f64820c253cd7364c4608a707
greg <greg.nwosu@gmail.com>
2023-08-01 01:14:23 +0100

    bing services

--- terraform/bing.tf
+++ terraform/bing.tf
@@ -1,9 +1,8 @@
 
-
 resource "azurerm_cognitive_account" "bing_search" {
   name                = "bing-search"
   location            = azurerm_resource_group.LangChain-Experiments.location
   resource_group_name = azurerm_resource_group.LangChain-Experiments.name
-  kind = "Bing.Search.v7"
+  kind                = "Bing.Search.v7"
   sku_name            = "F0"
 }
