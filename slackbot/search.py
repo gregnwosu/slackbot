646745fba1bcce78fbb46b1249afb1d538ee5f29
greg <greg.nwosu@gmail.com>
2023-08-05 04:28:31 +0100

    fix

--- slackbot/search.py
+++ slackbot/search.py
@@ -24,6 +24,6 @@ async def search_bing(query):
     headers = {"Ocp-Apim-Subscription-Key": primary_access_key}
     params = {"q": query, "mkt": mkt}
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"{endpoint}bing/v7.0/search", headers=headers, params=params) as response:
+        async with session.get(f"{endpoint}v7.0/search", headers=headers, params=params) as response:
             response.raise_for_status()
             return await response.json()
