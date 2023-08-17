f56dcf6cfa1c2b58893e8330ce916102e0b30341
greg <greg.nwosu@gmail.com>
2023-08-17 19:37:14 +0100

    speech2txt

--- slackbot/speak.py
+++ slackbot/speak.py
@@ -12,6 +12,47 @@ import azure.cognitiveservices.speech as speechsdk
 
 load_dotenv(find_dotenv())
 
+import azure.cognitiveservices.speech as speechsdk
+
+@dataclass
+class InMemoryStream(speechsdk.audio.PullAudioInputStreamCallback):
+    _buffer: bytes
+    _position: int = 0
+
+    def read(self, size: int) -> bytes:
+        chunk, self._position = self._buffer[self._position:self._position + size], self._position + size
+        return chunk
+
+    def close(self):
+        pass
+
+    @property
+    def audio_config(self) -> speechsdk.audio.AudioConfig:
+        return speechsdk.audio.AudioConfig(stream=speechsdk.audio.PullAudioInputStream(self))
+
+    async def speech_to_text(self) -> str:
+        primary_access_key = await get_secret("slackbot-synth-primary-access-key")
+        endpoint = await get_secret("slackbot-synth-endpoint")
+        
+        # Create a speech configuration
+        speech_config = speechsdk.SpeechConfig(subscription=primary_access_key, endpoint=endpoint)
+        
+        # Create a speech recognizer
+        recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=self.audio_config)
+        
+        # Recognize the speech from the audio stream
+        result = await recognizer.recognize_once_async()
+        
+        # Check the result for any errors and return the recognized text
+        if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            return result.text
+        elif result.reason == speechsdk.ResultReason.NoMatch:
+            return "No speech could be recognized."
+        elif result.reason == speechsdk.ResultReason.Canceled:
+            cancellation_details = result.cancellation_details
+            return f"Speech Recognition canceled: {cancellation_details.reason}. {cancellation_details.error_details}"
+
+
 async def text_to_speech(text: str) -> bytes:
     primary_access_key = await get_secret("slackbot-synth-primary-access-key")
     endpoint = await get_secret("slackbot-synth-endpoint")
