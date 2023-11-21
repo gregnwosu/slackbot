429cd886b23fa3f093cedb5ff681e02fb4fa2e3a
Gregory Nwosu <193151+gregnwosu@users.noreply.github.com>
2023-11-21 23:10:01 +0000

    remove autogen imports and usage from Agents file (#78)

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,11 +1,9 @@
 resource "azurerm_service_plan" "serviceplan" {
   name                = "serviceplan"
-  sku_name            = "F1"
+  sku_name            = "B1"
   os_type             = "Linux"
   location            = var.resource_group_location
   resource_group_name = "langchain-experiments"
-
-
 }
 
 resource "azurerm_linux_web_app" "slackbotwebapp" {
@@ -16,20 +14,20 @@ resource "azurerm_linux_web_app" "slackbotwebapp" {
   service_plan_id     = azurerm_service_plan.serviceplan.id
   app_settings = {
     "SCM_DO_BUILD_DURING_DEPLOYMENT" = "1"
-    "SLACK_BOT_TOKEN"                = "${var.SLACK_BOT_TOKEN}"
-    "SLACK_SIGNING_SECRET"           = "${var.SLACK_SIGNING_SECRET}"
-    "SLACK_BOT_USER_ID"              = "${var.SLACK_BOT_USER_ID}"
-    "SERPAPI_API_KEY"                = "${var.SERPAPI_API_KEY}"
-    "OPENAI_API_KEY"                 = "${var.OPENAI_API_KEY}"
-    "HUGGING_FACE_API_KEY"           = "${var.HUGGING_FACE_API_KEY}"
-    "VAR_ZAPIER_API_KEY"             = "${var.ZAPIER_API_KEY}"
-    "ELEVENLABS_API_KEY"             = "${var.ELEVENLABS_API_KEY}"
-    "REDIS_KEY"                      = "${var.REDIS_KEY}"
-    "REDIS_URL"                      = "${var.REDIS_URL}"
-    "VAULT_URL"                      = "${var.VAULT_URL}"
-    "ARM_CLIENT_ID"                  = "${var.ARM_CLIENT_ID}"
-    "ARM_CLIENT_SECRET"              = "${var.ARM_CLIENT_SECRET}"
-    "ARM_TENANT_ID"                  = "${var.ARM_TENANT_ID}"
+    "SLACK_BOT_TOKEN"                = var.SLACK_BOT_TOKEN
+    "SLACK_SIGNING_SECRET"           = var.SLACK_SIGNING_SECRET
+    "SLACK_BOT_USER_ID"              = var.SLACK_BOT_USER_ID
+    "SERPAPI_API_KEY"                = var.SERPAPI_API_KEY
+    "OPENAI_API_KEY"                 = var.OPENAI_API_KEY
+    "HUGGING_FACE_API_KEY"           = var.HUGGING_FACE_API_KEY
+    "VAR_ZAPIER_API_KEY"             = var.ZAPIER_API_KEY
+    "ELEVENLABS_API_KEY"             = var.ELEVENLABS_API_KEY
+    "REDIS_KEY"                      = var.REDIS_KEY
+    "REDIS_URL"                      = var.REDIS_URL
+    "VAULT_URL"                      = var.VAULT_URL
+    "ARM_CLIENT_ID"                  = var.ARM_CLIENT_ID
+    "ARM_CLIENT_SECRET"              = var.ARM_CLIENT_SECRET
+    "ARM_TENANT_ID"                  = var.ARM_TENANT_ID
 
     #get variable from the environment
 
