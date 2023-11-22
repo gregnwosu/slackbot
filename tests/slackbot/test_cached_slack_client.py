16648e0697921572827c2c27622ee118eb89b64d
Greg <greg.nwosu@gmail.com>
2023-11-22 22:33:19 +0000

    check that slack client is cached

--- tests/slackbot/test_cached_slack_client.py
+++ tests/slackbot/test_cached_slack_client.py
@@ -1,7 +1,7 @@
 from slackbot.app import cached_slack_client
 
 
-def test_returns_same_instance_on_subsequent_calls(self):
+def test_returns_same_instance_on_subsequent_calls():
     client1 = cached_slack_client()
     client2 = cached_slack_client()
     assert client1 is client2
