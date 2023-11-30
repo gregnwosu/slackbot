40ae0ff9a3cedd0b667ef7326cddf890f0b4ba91
Gregory Nwosu <193151+gregnwosu@users.noreply.github.com>
2023-11-30 19:11:45 +0000

    Update resources.tf (#118)

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -40,7 +40,7 @@ resource "azurerm_linux_web_app" "slackbotwebapp" {
     always_on        = false
     app_command_line = "gunicorn -w 2 -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker slackbot.app:api"
     application_stack {
-      python_version = "3.10"
+      python_version = "3.9"
     }
   }
   logs {
