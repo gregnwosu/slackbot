c4e571bde6c1a33ea1f2b8ab699dd81772ecfdc9
Gregory Nwosu <193151+gregnwosu@users.noreply.github.com>
2023-12-04 16:05:09 +0000

    try agency swarm (#131)

--- tests/slackbot/test_agency.py
+++ tests/slackbot/test_agency.py
@@ -1,19 +1,15 @@
-
 import pytest, asyncio
 from dotenv import load_dotenv, find_dotenv
 from slackbot import agent
 
-
 import os
 
 
-
 def test_agency():
-
     prompt = "Can you please find out what legal action i can take if my ex locked me out of my house?"
     conversation = agent.agency.get_completion(prompt)
 
-    # for msg_output in conversation:
-    #     print(msg_output)
-    #     msg_output.cprint()
-    #     print(msg_output.content)
+    for msg_output in conversation:
+        print(msg_output)
+        msg_output.cprint()
+        print(msg_output.content)
