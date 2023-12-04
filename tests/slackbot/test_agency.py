a9f033b994e31143767580742f574e76add8af6c
greg <greg.nwosu@gmail.com>
2023-12-04 12:56:54 +0000

    try agency swarm

--- tests/slackbot/test_agency.py
+++ tests/slackbot/test_agency.py
@@ -5,13 +5,15 @@ from slackbot import agent
 
 
 import os
+
+
+
 def test_agency():
 
     prompt = "Can you please find out what legal action i can take if my ex locked me out of my house?"
-    message_output_iterator = agent.agency.get_completion(prompt)
+    conversation = agent.agency.get_completion(prompt)
 
-    for i in range(5):
-        msg_output = next(message_output_iterator)
+    for msg_output in conversation:
         print(msg_output)
         msg_output.cprint()
         print(msg_output.content)
